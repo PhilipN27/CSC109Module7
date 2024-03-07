d1e5c43deb8aabe7b82d94b6568d145451175fc4
Philip <panora@quinnipiac.edu>
2024-03-06 19:36:49 -0500

    interaction with character who has multiple riddle options

--- src/TextBasedAdventure.java
+++ src/TextBasedAdventure.java
@@ -1,12 +1,7 @@
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class TextBasedAdventure {
     boolean hasSword = false;
-    List<String> inventory = new ArrayList<>();
-    int health = 100;
     Scanner keyboardInput = new Scanner(System.in);
 
     public void execute() {
@@ -17,12 +12,14 @@ public class TextBasedAdventure {
     }
 
     public void start() {
-        System.out.println("You find yourself in a large room. What would you like to do?\n1. Go left \n2. Go right");
+        System.out.println("You find yourself in a large room. What would you like to do?\n1. Go left \n2. Go right \n3. Talk to the mysterious figure");
         int input = keyboardInput.nextInt();
         if (input == 1) {
             goLeft();
         } else if (input == 2) {
             goRight();
+        } else if (input == 3) {
+            interactWithCharacter();
         }
     }
 
@@ -40,7 +37,6 @@ public class TextBasedAdventure {
         if (!hasSword) {
             System.out.println("You find a sword on the ground!");
             hasSword = true;
-            pickUpItem("sword");
         } else {
             System.out.println("There's nothing here...");
         }
@@ -52,37 +48,19 @@ public class TextBasedAdventure {
             System.out.println("You defeat the giant with your sword and run out of the cave!");
         } else {
             System.out.println("You get stomped by the giant and red stuff goes everywhere.");
-            health -= 50;
-            if (health <= 0) {
-                System.out.println("You have died.");
-                // End game or restart
-            } else {
-                System.out.println("You have " + health + " health left.");
-                // Allow player to use a health potion if available
-                if (useItem("health potion")) {
-                    health += 30;
-                    System.out.println("You have " + health + " health now.");
-                }
-                start();
-            }
         }
     }
 
-    public void pickUpItem(String item) {
-        System.out.println("You found a " + item + "!");
-        inventory.add(item);
-        System.out.println("Inventory: " + inventory);
-    }
-
-    public boolean useItem(String item) {
-        if (inventory.contains(item)) {
-            inventory.remove(item);
-            System.out.println(item + " used.");
-            return true;
+    public void interactWithCharacter() {
+        System.out.println("A mysterious figure appears before you. 'Answer my riddle and I shall grant you a clue to escape this cave,' it says.");
+        System.out.println("'I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?'");
+        String answer = keyboardInput.next();
+        if ("echo".equalsIgnoreCase(answer)) {
+            System.out.println("The figure nods approvingly. 'You are wise. Look for the lever hidden behind the stone in the next room.'");
         } else {
-            System.out.println("You don't have a " + item + ".");
-            return false;
+            System.out.println("The figure shakes its head. 'That is incorrect. You may try again later.'");
         }
+        start();
     }
 
     public static void main(String[] args) {
